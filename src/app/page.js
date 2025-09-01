bb63bfd4a635af60518f340bbc1d5dd4e6c0c91b
ARVINDSHUKLA0 <shuklaarvind2521@gmail.com>
2025-09-01 15:50:44 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
           <div
             key={cat.id}
             className="col-lg-3 col-md-4 col-sm-6 col-12 px-1"
-          > 
+          >
             <div className={styles.categoryItem} style={{ position: "relative" }}>
               <div className={`d-flex ${styles.contentWrap}`}>
                 <h6 className={styles.textContent}>
@@ -49,7 +49,7 @@ export default function Home() {
                 >
                   {expanded === cat.id ? "−" : "+"}
                 </p>
-              </div> 
+              </div>
               {expanded === cat.id && (
                 <div className={styles.expandTop}>
                   <p>
@@ -58,7 +58,7 @@ export default function Home() {
                   </p>
                 </div>
               )}
-                <img
+              <img
                 src={cat.thumbnail}
                 alt={`Category ${cat.id}`}
                 onClick={() => openGallery(cat.products)}
@@ -78,8 +78,87 @@ export default function Home() {
           </button>
         </div>
       )}
+      <div className="container pt-md-3 pb-md-5 py-2">
+        <div className="row justify-content-center align-items-stretch g-4">
 
-      <Footer/>
+          <div className="col-md-4 d-flex my-md-4 py-2 pt-3">
+            <div className="card text-center border-0 shadow rounded-4 flex-fill">
+              <div className="fw-bold rounded-top-4 py-3" style={{backgroundColor : "#d3d3d3b9"}}>
+                TRENDING PLAN
+              </div>
+              <div className="card-body d-flex flex-column">
+                <div>
+                  <img
+                    src="/assets/img/blidsvg.svg"
+                    alt="Solo Walks"
+                    style={{ height: "50px" }}
+                  />
+                </div>
+                <h4 className="fw-bold mb-2">Craft competition</h4>
+                 
+                  <p className="m-0 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+
+                <h5 className="w-bold mb-2">Event Info</h5>
+                <p className="px-3 p-0">Your content goes here. Edit or remove this text inline or in the module Content settings.</p>
+                <button className="btn btn-outline-dark rounded-pill fw-bold px-4 mt-auto mb-2">
+                  BOOK NOW!
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 d-flex">
+            <div className="card text-center border-0 shadow-sm rounded-4 flex-fill">
+              <div className="bg-dark text-white fw-bold rounded-top-4 py-3">
+                POPULAR PLAN
+              </div>
+              <div className="card-body d-flex flex-column">
+                <div className="mb-3">
+                  <img
+                    src="/assets/img/one.svg"
+                    alt="Group Walks"
+                    style={{ height: "60px" }}
+                  />
+                </div>
+                <h4 className="fw-bold mb-2">Parent Teach Conferences</h4>
+                 <p className="m-0 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                 <h5>Event Info</h5>
+                 <p className="px-3">Your content goes here. Edit or remove this text inline or in the module Content settings. </p>
+                <button className="btn btn-dark rounded-pill fw-bold px-4 mt-auto">
+                  BOOK NOW !
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 d-flex my-md-4 py-2 pt-3">
+            <div className="card text-center border-0 shadow-sm rounded-4 flex-fill">
+              <div className="fw-bold rounded-top-4 py-3" style={{backgroundColor : "#d3d3d3b9"}}>
+                PREMIUM PLAN
+              </div>
+              <div className="card-body d-flex flex-column">
+                <div>
+                  <img
+                    src="/assets/img/blidsvg.svg"
+                    alt="Solo Walks"
+                    style={{ height: "50px" }}
+                  />
+                </div>
+                <h4 className="fw-bold mb-2">Craft competition</h4>
+                 
+                  <p className="m-0 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+
+                <h5 className="w-bold mb-2">Event Info</h5>
+                <p className="px-3 p-0">Your content goes here. Edit or remove this text inline or in the module Content settings. </p>
+                <button className="btn btn-outline-dark rounded-pill fw-bold px-4 mt-auto mb-2">
+                  BOOK NOW!
+                </button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
